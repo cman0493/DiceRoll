dd05d34e0d34fc015fde781d5b5aa3f30fa90aa8
Chris Hunley <cshunley21@gmail.com>
2021-10-06 09:50:41 -0500

    Added animation

--- dice.js
+++ dice.js
@@ -1,21 +1,20 @@
-var dice = {
-    sides: 6,
-    roll: function () {
-      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-      return randomNumber;
-    }
-  }
-  
-  function printNumber(number) {
-    var placeholder = document.getElementById('placeholder');
-    placeholder.innerHTML = number;
-  }
-  
-  var button = document.getElementById('button');
-  
-  button.onclick = function() {
-    var result = dice.roll();
-    printNumber(result);
-  };
-  
-  
+function rollDice() {
+  const dice = [...document.querySelectorAll(".die-list")];
+  dice.forEach(die => {
+    toggleClasses(die);
+    die.dataset.roll = getRandomNumber(1, 6);
+  });
+}
+
+function toggleClasses(die) {
+  die.classList.toggle("odd-roll");
+  die.classList.toggle("even-roll");
+}
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.getElementById("roll-button").addEventListener("click", rollDice);
